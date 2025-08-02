bf1bde70e08350e9e281c0e5b84ea3bedaaeee2a
ShaikIrfawn <hackinglabprojectz@gmail.com>
2025-08-02 13:14:53 +0800

    halfway

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -3,18 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('nav ul li a');
 
   for (const link of links) {
-    link.addEventListener('click', smoothScroll);
-  }
-
-  function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-    if (targetSection) {
-      window.scrollTo({
-        top: targetSection.offsetTop - 60,
-        behavior: 'smooth'
-      });
-    }
+    link.addEventListener('click', function(event) {
+      const href = this.getAttribute('href');
+      if (href && href.startsWith('#')) {
+        event.preventDefault();
+        const targetId = href.substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+          window.scrollTo({
+            top: targetSection.offsetTop - 60,
+            behavior: 'smooth'
+          });
+        }
+      }
+    });
   }
 });
